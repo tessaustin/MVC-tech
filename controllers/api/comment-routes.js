9ef1b95c8070a655c77a9fc26e79902b5f08485b
Tess Austin <tessaustin92@gmail.com>
2021-04-25 18:27:30 -0600

    update server, temp css., trying ot get web browser to load but 404

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -10,7 +10,7 @@ router.get('/', (req, res) => {
         });
 });
 
-router.post('/', withAuth, (req, res) => {
+router.post('/', (req, res) => {
     if (req.session) {
         Comment.create({
             comment_text: req.body.comment_text,
