ff110fe10cfcd1cd27019fd1d935501ac25cccc4
Tess Austin <tessaustin92@gmail.com>
2021-04-26 20:26:03 -0600

    when creating post-content now saves

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -76,7 +76,7 @@ router.get('/:id', (req, res) => {
 router.post('/', withAuth, (req, res) => {
     Post.create({
         title: req.body.title,
-        post_content: req.body.post_url,
+        post_content: req.body.post_content,
         user_id: req.session.user_id
     })
         .then(dbPostData => res.json(dbPostData))
