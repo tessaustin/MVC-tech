8f0f68fc092a67aab8534705c3251005304d3725
Tess Austin <tessaustin92@gmail.com>
2021-04-26 19:41:29 -0600

    create post handlebar, post will create but no content will load just the title

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -68,7 +68,7 @@ router.get('/edit/:id', withAuth, (req, res) => {
     .then(dbPostData => {
       if (dbPostData) {
         const post = dbPostData.get({ plain: true });
-        
+
         res.render('edit-post', {
           post,
           loggedIn: true
@@ -78,6 +78,42 @@ router.get('/edit/:id', withAuth, (req, res) => {
       }
     })
     .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.get('/create/', withAuth, (req, res) => {
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id
+    },
+    attributes: [
+      'id',
+      'title',
+      'created_at',
+      'post_content'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('create-post', { posts, loggedIn: true });
+    })
+    .catch(err => {
+      console.log(err);
       res.status(500).json(err);
     });
 });
